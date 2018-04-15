e53484c22cff3cf07c66495282214aef3b4381dd
Chris Min <chris.min@careerbuilder.com>
2018-04-15 12:24:07 -0400

    Finishing join example

--- src/main/scala/chrism/sdsc/join/joins.scala
+++ src/main/scala/chrism/sdsc/join/joins.scala
@@ -1,15 +1,11 @@
 package chrism.sdsc.join
 
-import org.apache.spark.sql.{Column, SparkSession, functions}
+import chrism.sdsc.Runner
+import org.apache.spark.sql._
 
-object JoinExamples {
+object JoinExamples extends Runner {
 
-  def main(args: Array[String]): Unit = {
-    implicit val spark: SparkSession = SparkSession.builder()
-      .appName("Spam Detector Model Creator")
-      .master("local[*]")
-      .getOrCreate()
-
+  override def run(args: Array[String])(implicit spark: SparkSession): Unit = {
     import spark.implicits._
 
     val nameTable = spark.createDataset(Seq(
@@ -36,30 +32,47 @@ object JoinExamples {
       Employment(5, null),
       Employment(6, "Paleontologist")))
 
-//    val joinCol = functions.col("id")
-
-    // Dataset inner join
+    // Dataset INNER JOIN
     val nameGenderDs = nameTable
       .joinWith(
         genderTable,
         nameTable("id") === genderTable("id")
-        /*inner join by default*/) // Dataset[(Name, Gender)]
-         .map(r => Profile(r._1.id, first = r._1.first, last = r._1.last, gender = r._2.gender))
-//      .map(Profile(_))
+        /*inner join by default*/)
+      .map(r => Profile(r._1.id, first = r._1.first, last = r._1.last, gender = r._2.gender))
 
-//
-//      .show(6, truncate = false)
-//
-//
-//    // Dataset inner join
-    nameGenderDs
-      .joinWith(
-        employmentTable.flatMap(p => Seq(p, p.copy())),
-        functions.col("id")/*nameGenderDs("id") === employmentTable("id")*/,
-        "leftOuter")
-         .map(r => Profile(r._1.id, first = r._1.first, last = r._1.last, gender = r._1.gender, jobTitle = r._2.jobTitle))
-//      .map(Profile(_))
-      .show(6, truncate = false)
+    nameGenderDs.show(6, truncate = false)
+
+    // Dataset LEFT OUTER JOIN
+    import PimpMyJoins._
+
+    val nonNullEmploymentDs = employmentTable
+      .filter(_.jobTitle != null)
+
+    val profileDs = nameGenderDs
+      .leftOuterJoin(
+      nonNullEmploymentDs,
+      nameGenderDs("id") === nonNullEmploymentDs("id"))(
+        // LEFT OUTER JOIN can result in right record being null
+      (l, r) => if (r == null) l else l.copy(jobTitle = r.jobTitle))
+
+    profileDs.show(6, truncate = false)
+
+    // Try FULL OUTER JOIN
+    // Note that as opposed to LEFT OUTER JOIN, either left or right record can be null in a FULL OUTER JOIN.
+  }
+
+  private object PimpMyJoins {
+
+    // Just to demo so-call "pimp-my-library" pattern
+
+    implicit final class Joiner[L](leftDs: Dataset[L]) {
+
+      def leftOuterJoin[R, J: Encoder](rightDs: Dataset[R], condition: Column)
+        (joinFunc: (L, R) => J): Dataset[J] = {
+        leftDs.joinWith(rightDs, condition, "leftOuter").map(joinFunc.tupled)
+      }
+    }
+
   }
 
 }
@@ -70,10 +83,9 @@ final case class Gender(id: Int, gender: String)
 
 final case class Employment(id: Int, jobTitle: String)
 
-final case class Profile(id: Int, first: String = null, last: String = null, gender: String = null, jobTitle: String = null)
-
-object Profile {
-
-  def apply(nameGender: (Name, Gender)): Profile =
-    Profile(nameGender._1.id, first = nameGender._1.first, last = nameGender._1.last, gender = nameGender._2.gender)
-}
+final case class Profile(
+    id: Int,
+    first: String = null,
+    last: String = null,
+    gender: String = null,
+    jobTitle: String = null)
